1cd629470ea876a9a6f82db1f382d005a5bb7773
lei0928 <43226184+lei0928@users.noreply.github.com>
2019-04-20 10:22:00 -0700

    Create dragon.h

---
+++ Program 2/dragon.h
@@ -0,0 +1,36 @@
+/*
+ * dragon.h
+ *
+ *  Created on: Oct 22, 2018
+ *      Author: william
+ */
+
+// Dragon Class
+#ifndef DRAGON_H_
+#define DRAGON_H_
+
+#include "Turtle.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//-------------------------------------------------------------------
+// Dragon curve:
+//    -- leftCurve: draws a left Dragon arrowhead at a given level
+//    -- rightCurve: draws a right Dragon arrowhead at a given level
+//
+// Assumptions:
+//    -- Level 1 is defined as an integer.
+//    -- Distance d is define as a float.
+//    -- Outputs are in postscript format.
+//-------------------------------------------------------------------
+
+class Dragon : Turtle {
+public:
+	Dragon(float initX=0.0, float initY=0.0, float initAngle=0.0);
+	void leftCurve(int l, float d);  // draw a level-1 left curve with distance d
+	void rightCurve(int l, float d); // draw a level-1 right curve with distance d
+};
+
+#endif /* DRAGON_H_ */
